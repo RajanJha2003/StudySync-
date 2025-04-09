9fb4d277e7cc0a5bc9563aed3626e8133e756fe7
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-04-09 10:34:06 +0530

    user schema

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,15 @@ const PORT = process.env.PORT || 5000;
 
 connectDb();
 
+// Default Route
+app.get('/', (req, res) => {
+    // console.log('Your server is up and running..!');
+    res.send(`<div>
+    This is Default Route  
+    <p>Everything is OK</p>
+    </div>`);
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
