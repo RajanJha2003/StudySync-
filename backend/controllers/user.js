31b8979bcc17dd8c1a3f78bb6d4a0d9019994202
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-04-09 11:02:45 +0530

    otp model

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,29 @@
 
 
 const signup=async(req,res)=>{
+   try {
+    const { firstName, lastName, email, password, confirmPassword,
+        accountType, contactNumber, otp } = req.body;
+        if (!firstName || !lastName || !email || !password || !confirmPassword || !accountType || !otp) {
+            return res.status(401).json({
+                success: false,
+                message: 'All fields are required..!'
+            });
+        }
 
+        if(password!=confirmPassword){
+            return res.status(400).json({
+                success:false,
+                message:"Password and confirm password did not match , please try again"
+            })
+        }
+
+        
+   } catch (error) {
+    
+   }
+    
 }
 
 
-export default signup;
+export {signup};
---
+++ backend/model/otp.js
@@ -0,0 +1,22 @@
+
+import mongoose from "mongoose";
+
+const otpSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    otp:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now(),
+        expires:5*60  // the otp will automatically delete after 5 minutes of creation
+    }
+})
+
+const OTP=mongoose.model("OTP",otpSchema);
+
+export default OTP;
