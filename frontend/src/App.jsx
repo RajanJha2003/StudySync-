8ad9795b664873d3be6427323f21442d614a5aee
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-04-12 18:50:26 +0530

    signup operations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,17 @@ import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Navbar from './components/common/Navbar'
+import VerifyEmail from './pages/VerifyEmail'
 
 const App = () => {
+  
   return (
     <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter'>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/verify-email" element={<VerifyEmail />}  />
         
       </Routes>
     </div>
--- frontend/src/services/operations/authAPI.js
+++ frontend/src/services/operations/authAPI.js
@@ -44,3 +44,37 @@ const {
       toast.dismiss(toastId);
     }
   }
+
+
+  export function signUp(accountType, firstName, lastName, email, password, confirmPassword, otp, navigate) {
+    return async (dispatch) => {
+      const toastId = toast.loading("Loading...");
+      dispatch(setLoading(true));
+      try {
+        const response=await apiConnector("POST",SIGNUP_API,{
+          accountType,
+          firstName,
+          lastName,
+          email,
+          password,
+          confirmPassword,
+          otp,
+        })
+
+        if(!response.data.success){
+          toast.error(response.data.message);
+          throw new Error(response.data.message);
+        }
+
+        toast.success("Signup Successful");
+        navigate("/login");
+      } catch (error) {
+        console.log("SIGNUP API ERROR ---> ", error);
+       
+          toast.error("Invalid OTP");
+        
+      }
+      dispatch(setLoading(false));
+      toast.dismiss(toastId);
+    } 
+  }
