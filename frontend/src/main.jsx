0b831311a95b693d6f0e351f0d8d72213292130e
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-04-12 14:02:49 +0530

    redux added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './reducer/index.js'
+
+const store=configureStore({
+  reducer:rootReducer
+})
 
 createRoot(document.getElementById('root')).render(
  <BrowserRouter>
+  <Provider store={store}>
   <StrictMode>
     <App />
   </StrictMode>,
+  </Provider>
  </BrowserRouter>
 )
